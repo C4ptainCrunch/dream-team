7f8c445f541d00bfe27bd7ac72326d7f945389a5
Nikita Marchant <nikita.marchant@gmail.com>
2016-02-29 11:02:07 +0100

    First window

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,9 +1,21 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 /**
  * Created by nikita on 2/29/16.
  */
 public class GUI extends JFrame{
+    public GUI() {
+        super();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // http://docs.oracle.com/javase/tutorial/uiswing/components/toplevel.html
+        JPanel contentPane = new JPanel(new BorderLayout());
+
+        this.pack();
+        this.setVisible(true);
+        setTitle("Test");
+    }
 }

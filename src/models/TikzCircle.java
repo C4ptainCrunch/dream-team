db8b34ad9ce3b0a41c6a88c8466919d02463e5d9
Romain Fontaine <romain-fontaine@outlook.com>
2016-02-29 19:26:59 +0100

    [add] test for TikzedCircle

--- src/models/TikzCircle.java
+++ src/models/TikzCircle.java
@@ -1,9 +1,21 @@
 package models;
 
+import constants.Models;
+
 import java.awt.*;
 
 public class TikzCircle extends TikzShape {
+    private int radius;
     public TikzCircle(){
         super();
+        radius = Models.DEFAULT.LENGTH;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
     }
 }

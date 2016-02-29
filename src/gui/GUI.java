8dac672727ae4b4cc61ab5b1b94ccf17567c01f7
Nikita Marchant <nikita.marchant@gmail.com>
2016-02-29 12:40:20 +0100

    Add a split panel to the main window

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,16 +3,25 @@ package gui;
 import javax.swing.*;
 import java.awt.BorderLayout;
 
-/**
- * Created by nikita on 2/29/16.
- */
+
 public class GUI extends JFrame{
     public GUI() {
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // http://docs.oracle.com/javase/tutorial/uiswing/components/toplevel.html
-        JPanel contentPane = new JPanel(new BorderLayout());
+        JPanel window = new JPanel(new BorderLayout());
+
+        JPanel drawingPanel = new JPanel();
+        JPanel texPanel = new JPanel();
+
+        JSplitPane splitPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, drawingPanel, texPanel);
+        splitPanel.setResizeWeight(0.5);
+
+        window.add(splitPanel);
+
+        // add the window to the JFrame
+        this.setContentPane(window);
 
         this.pack();
         this.setVisible(true);

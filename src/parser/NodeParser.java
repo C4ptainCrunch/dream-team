314ff2beac89a52ee8b4c8e24d990e65f05e0053
Anthony Caccia <acaccia@ulb.ac.be>
2016-03-01 12:25:19 +0100

    options parser (+test)

--- src/parser/NodeParser.java
+++ src/parser/NodeParser.java
@@ -1,5 +1,8 @@
 package parser;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.codehaus.jparsec.Parser;
 import org.codehaus.jparsec.Parsers;
 import org.codehaus.jparsec.Scanners;
@@ -14,5 +17,11 @@ public class NodeParser {
 	public static Parser<String> label() {
 		return Patterns.regex("[^}]").many().toScanner("name string")
 				.between(Scanners.isChar('{'), Scanners.isChar('}')).source().map(s -> s.substring(1, s.length() - 1));
+	}
+
+	public static Parser<List<String>> options() {
+		return Patterns.regex("[^]]").many().toScanner("options string")
+				.between(Scanners.isChar('['), Scanners.isChar(']')).source()
+				.map(s -> Arrays.asList(s.substring(1, s.length() - 1).split(", *")));
 	}
 }
--- src/parser/NodeParserTest.java
+++ src/parser/NodeParserTest.java
@@ -1,6 +1,9 @@
 package parser;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
+import org.junit.Test;
 
 public class NodeParserTest {
 
@@ -12,5 +15,16 @@ public class NodeParserTest {
 	@org.junit.Test
 	public void testLabel() throws Exception {
 		Assert.assertEquals(NodeParser.label().parse("{Hello, my dear little world}"), "Hello, my dear little world");
+	}
+
+	@Test
+	public void testOptions() throws Exception {
+		ArrayList<String> testarray = new ArrayList<>();
+		testarray.add("hello");
+		testarray.add("my");
+		testarray.add("name");
+		testarray.add("is");
+		testarray.add("trololo");
+		Assert.assertEquals(NodeParser.options().parse("[hello, my,name, is,   trololo]"), testarray);
 	}
 }

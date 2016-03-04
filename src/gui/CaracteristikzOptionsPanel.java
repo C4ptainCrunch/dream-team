78aa3bbe94a4e90926fb58d72d0313faab7de1c3
Aurelien van Delft <avdelft@ulb.ac.be>
2016-03-04 21:46:06 +0100

    add stroke width fields

--- src/gui/CaracteristikzOptionsPanel.java
+++ src/gui/CaracteristikzOptionsPanel.java
@@ -3,29 +3,85 @@ package gui;
 
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class CaracteristikzOptionsPanel extends JPanel {
-    private static final int ROW_COUNT = 4;
+    private static final int ROW_COUNT = 5;
     private static final int COLUMN_COUNT = 1;
+    private static final int MIN_WIDTH = 1;
+    private static final int MAX_WIDTH = 100;
 
     private JButton color_chooser;
     private JLabel color_title;
-    private JLabel stroke_size_title;
+    private JLabel stroke_width_title;
+    private JSlider stroke_width_slider;
+    private JFormattedTextField stroke_width_entry;
 
     public CaracteristikzOptionsPanel(){
         this.setLayout(new GridLayout(ROW_COUNT, COLUMN_COUNT));
         this.setBorder(new EtchedBorder());
+
+        initColorOptions();
+        initStrokeOptions();
+    }
+
+    private void initColorOptions(){
         initColorTitle();
         initColorChooser();
         addColorChooserListener();
     }
 
+    private void initStrokeOptions(){
+        initStrokeWidthTitle();
+        initStrokeWidthEntry();
+        initStrokeWidthSlider();
+        addStrokeWidthEntryListener();
+    }
+
     private void initColorTitle(){
         color_title = new JLabel("<HTML><U>Stroke Color</U></HTML>");
         this.add(color_title, 0);
+    }
+
+    private void initStrokeWidthSlider(){
+        stroke_width_slider = new JSlider(MIN_WIDTH, MAX_WIDTH, MIN_WIDTH);            // Third arg is initial value.
+        this.add(stroke_width_slider, 4);
+        stroke_width_slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                int value = stroke_width_slider.getValue();
+                stroke_width_entry.setText(Integer.toString(value));
+            }
+        });
+    }
+
+    private void initStrokeWidthEntry(){
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
+        numberFormat.setGroupingUsed(false);
+        stroke_width_entry = new JFormattedTextField(numberFormat);
+        stroke_width_entry.setText(Integer.toString(MIN_WIDTH));
+        this.add(stroke_width_entry, 3);
+    }
+
+    private void addStrokeWidthEntryListener(){
+        stroke_width_entry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String value = stroke_width_entry.getText();
+                stroke_width_slider.setValue(Integer.parseInt(value));
+            }
+        });
+    }
+
+    private void initStrokeWidthTitle(){
+        stroke_width_title = new JLabel("<HTML><U>Stroke Width</U></HTML>");
+        this.add(stroke_width_title, 2);
     }
 
     private void initColorChooser(){

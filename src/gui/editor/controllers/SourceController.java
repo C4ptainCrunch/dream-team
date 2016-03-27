773f25bcc7e52f97323804c4f7538079cd70b6f1
Nikita Marchant <nikita.marchant@gmail.com>
2016-03-27 10:47:12 +0200

    Replace println with logger.*

--- src/gui/editor/controllers/SourceController.java
+++ src/gui/editor/controllers/SourceController.java
@@ -7,10 +7,12 @@ import org.codehaus.jparsec.error.ParserException;
 import parser.NodeParser;
 
 import javax.swing.*;
+import java.util.logging.Logger;
 
 public class SourceController {
     private SourceView view;
     private TikzGraph graph;
+    private static final Logger logger = Logger.getLogger("gui.editor.controllers.source");
 
     public SourceController(SourceView view, TikzGraph graph) {
         this.view = view;
@@ -18,21 +20,19 @@ public class SourceController {
     }
 
     public void updateFromText(String text) {
-        if(text.trim().length() != 0) {
+        text = text.trim();
+        if(text.length() != 0) {
             TikzGraph new_graph = new TikzGraph();
-            System.out.println("TEXT\n-------");
-            System.out.println(text);
-            System.out.println("-------");
+            logger.fine("TikZ updated event. New TikZ : " + text);
             try {
                 NodeParser.parseDocument(new_graph).parse(text);
+                logger.fine("Valid graph from TikzSource : " + graph.getNodes().size() + " nodes");
                 SwingUtilities.invokeLater(() -> {
                     graph.replace(new_graph);
-                    System.out.println("Runnable done");
+                   logger.fine("Runnable done: graph replace");
                 });
-
-                System.out.println("Valid graph, " + graph.getNodes().size() + " nodes");
             } catch (ParserException e) {
-                System.out.println(e);
+                logger.warning("Error during TikZ parsing : " + e.getMessage());
             }
         }
     }

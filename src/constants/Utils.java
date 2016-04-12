3780c7329451af2a765cb870f7e696da5328d865
BamBalaam <amadeiracortes@gmail.com>
2016-04-12 16:08:37 +0200

    Saving project paths to application dir

--- src/constants/Utils.java
+++ src/constants/Utils.java
@@ -5,4 +5,8 @@ package constants;
  */
 public final class Utils {
     public static final String HISTORY_PATH = ".history";
+    public static final String LINUX_PATH = "/.local/share/CreaTiKz";
+    public static final String MAC_PATH = "/Library/Application Support/CreaTiKz";
+    public static final String WINDOWS_PATH_ONE = "C:\\Documents and Settings\\";
+    public static final String WINDOWS_PATH_TWO = "\\Application Data\\Local Settings\\GroupeUn\\CreaTiKz";
 }
--- src/gui/projectManagement/controllers/ProjectManagementController.java
+++ src/gui/projectManagement/controllers/ProjectManagementController.java
@@ -3,9 +3,15 @@ package gui.projectManagement.controllers;
 
 import gui.editor.views.EditorView;
 import gui.projectManagement.views.ProjectManagementView;
+import constants.Utils;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class ProjectManagementController {
     private ProjectManagementView view;
@@ -14,7 +20,51 @@ public class ProjectManagementController {
         this.view = view;
     }
 
+    private java.io.File getSavedPathsFileFromPath(String path){
+        String homeDir = System.getProperty("user.home");
+        path = homeDir + path;
+        String fileSeparator = System.getProperty("file.separator");
+
+        java.io.File savedDir = new java.io.File(path);
+        // Default for now, will receive current username when accounts exist.
+        java.io.File savedPathsFile = new java.io.File(path+fileSeparator+"default.paths");
+
+        System.out.println(path);
+        System.out.println(path+fileSeparator+"default.paths");
+
+        if(! savedDir.exists()){
+            savedDir.mkdirs();
+            try {
+                savedPathsFile.createNewFile();
+            } catch (IOException e) {  // Not logical but necessary
+                System.err.println("Exists: " + e.getMessage());
+            }
+        }
+
+        return savedPathsFile;
+    }
+
+    private java.io.File getSavedPathsFile(){
+        java.io.File savedPathsFile;
+
+        switch (System.getProperty("os.name")){
+            case "Linux":
+                savedPathsFile = getSavedPathsFileFromPath(Utils.LINUX_PATH);
+                break;
+            case "Mac OS X":
+                savedPathsFile = getSavedPathsFileFromPath(Utils.MAC_PATH);
+                break;
+            default:  // Unfortunately, default is Windows OS...
+                String windowsPath = Utils.WINDOWS_PATH_ONE + System.getProperty("user.name") + Utils.WINDOWS_PATH_TWO;
+                savedPathsFile = getSavedPathsFileFromPath(windowsPath);
+                break;
+        }
+
+        return savedPathsFile;
+    }
+
     public void createProject() {
+
         java.io.File f = createPanel("Choose location to create your project", JFileChooser.DIRECTORIES_ONLY);
         if (f != null){
             String filePath = f.getAbsolutePath();
@@ -22,6 +72,18 @@ public class ProjectManagementController {
             if(!f.exists()){
                 try{
                     f.mkdir();
+                    java.io.File savedPathsFile = getSavedPathsFile();
+
+                    try(FileWriter fw = new FileWriter(savedPathsFile, true);
+                        BufferedWriter bw = new BufferedWriter(fw)
+                        )
+                    {
+                        bw.append(filePath+"\r\n");
+                    }
+                    catch (IOException e) {
+
+                    }
+
                     java.awt.EventQueue.invokeLater(()->new EditorView(filePath, false));
                     view.dispose(); //Exit previous windows
                 }catch(SecurityException e){

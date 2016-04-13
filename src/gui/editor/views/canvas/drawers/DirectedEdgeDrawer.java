47350d26ca139f09a889621652a1739511125264
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-04-13 17:02:15 +0200

    [fix] Arrow head adjusts to arrow direction

--- src/gui/editor/views/canvas/drawers/DirectedEdgeDrawer.java
+++ src/gui/editor/views/canvas/drawers/DirectedEdgeDrawer.java
@@ -18,8 +18,8 @@ public class DirectedEdgeDrawer extends EdgeDrawer {
     public Vector<Drawable> toDrawable(){
         Vector<Drawable> vec = super.toDrawable();
 
-        Point start = this.getComponent().getFromPosition();
-        Point end = this.getComponent().getToPosition();
+        Point start = this.getComponent().getToPosition();
+        Point end = this.getComponent().getFromPosition();
         double eucDist = Math.sqrt(Math.pow(end.getX() - start.getX(), 2) + Math.pow(end.getY() - start.getY(), 2));
         double alpha = Math.asin(end.getY()/eucDist);
         double beta = alpha - ARROW_ANGLE;
@@ -28,8 +28,11 @@ public class DirectedEdgeDrawer extends EdgeDrawer {
         // First arrow head segment.
         double firstDeltaX = Math.cos(beta)*ARROW_LENGTH;
         double firstDeltaY = Math.sin(beta)*ARROW_LENGTH;
+        int corrX, corrY;
+        corrX = (end.getX() > start.getX()) ? -1 : 1;
+        corrY = (end.getY() > start.getY()) ? -1 : 1;
         Drawable firstHeadSegment = new DrawableShape(
-                new Line2D.Float(end, new Point((int) (end.getX() + firstDeltaX), (int)(end.getY() + firstDeltaY))),
+                new Line2D.Float(end, new Point((int) (end.getX() + firstDeltaX*corrX), (int)(end.getY() + firstDeltaY*corrY))),
                 new BasicStroke(2),
                 this.getComponent().getColor(),
                 this.getComponent().getColor()
@@ -39,7 +42,7 @@ public class DirectedEdgeDrawer extends EdgeDrawer {
         double secondDeltaX = Math.sin(gamma)*ARROW_LENGTH;
         double secondDeltaY = Math.cos(gamma)*ARROW_LENGTH;
         Drawable secondHeadSegment = new DrawableShape(
-                new Line2D.Float(end, new Point((int) (end.getX() + secondDeltaX), (int)(end.getY() + secondDeltaY))),
+                new Line2D.Float(end, new Point((int) (end.getX() + secondDeltaX*corrX), (int)(end.getY() + secondDeltaY*corrY))),
                 new BasicStroke(2),
                 this.getComponent().getColor(),
                 this.getComponent().getColor()

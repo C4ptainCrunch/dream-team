473cd89739708d4b0d07993aef437fcd821b7b21
Vander Marcken Raphaël <rvdmarck@ulb.ac.be>
2016-04-17 16:59:04 +0200

    Added javadoc for toolbox/views

--- src/gui/editor/toolbox/controllers/SelectorController.java
+++ src/gui/editor/toolbox/controllers/SelectorController.java
@@ -8,22 +8,40 @@ import gui.editor.toolbox.model.ToolModel;
 import gui.editor.toolbox.views.SelectorView;
 
 /**
- * Created by aurelien on 13/04/16.
+ * Implementation of the Controller (from the MVC architectural pattern) for the Selector.
+ * The Selector is part of the toolbox used to choose
+ * which component is selected.
  */
 public class SelectorController implements Observer {
 
     private final SelectorView view;
     private final ToolModel model;
 
+    /**
+     * Constructs a new Controller for the Selector
+     * with a given SelectorView and a ToolModel
+     * @param v The selector view which is associated with this controller
+     * @param m The tool model
+     */
     public SelectorController(SelectorView v, ToolModel m) {
         view = v;
         model = m;
     }
 
+    /**
+     * Informs the model that a component is being selected
+     * @param component The component being selected
+     */
     public void itemSelected(TikzComponent component) {
         model.setComponent(component);
     }
 
+    /**
+     * Called when Observables linked to this Observer call notify(),
+     *
+     * @param o The Observable
+     * @param obj The Object given by the Observable
+     */
     @Override
     public void update(Observable o, Object obj) {
         // this was left intentionally blank
--- src/gui/editor/toolbox/views/PreviewView.java
+++ src/gui/editor/toolbox/views/PreviewView.java
@@ -15,12 +15,19 @@ import gui.editor.views.canvas.drawables.DrawableTikzComponent;
 import gui.editor.views.canvas.drawers.Drawer;
 
 /**
- * Created by aurelien on 12/04/16.
+ * Implementation of the View (from the MVC architectural pattern) for the Preview.
+ * The Preview is part of the toolbox used to show
+ * a preview of the component being edited.
  */
 public class PreviewView extends JPanel {
     TikzComponent component;
     PreviewController controller;
 
+    /**
+     * Constructs a new view for the Preview
+     * with a given ToolModel
+     * @param model the tool model
+     */
     public PreviewView(ToolModel model) {
         this.setBackground(Color.WHITE);
         this.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
@@ -28,6 +35,9 @@ public class PreviewView extends JPanel {
         enableDrag();
     }
 
+    /**
+     * Enables the drag&drop action for the preview component
+     */
     private void enableDrag() {
         this.setTransferHandler(new TikzTransferHandler());
         this.addMouseListener(new MouseAdapter() {
@@ -40,14 +50,26 @@ public class PreviewView extends JPanel {
         });
     }
 
+    /**
+     * Getter for the component being previewed
+     * @return The component being previewed
+     */
     public TikzComponent getComponent() {
         return this.controller.getComponent();
     }
 
+    /**
+     * Setter for the component being previewed
+     * @param component the component to be set for the preview
+     */
     public void setComponent(TikzComponent component) {
         this.component = component;
     }
 
+    /**
+     * Paints the component being previewed
+     * @param g The Graphics to be painted
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (component != null) {

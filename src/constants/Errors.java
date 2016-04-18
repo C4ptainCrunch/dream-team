121d3c0f462e712f2dc507902cdf45845138fa87
Aurelien van Delft <avdelft@ulb.ac.be>
2016-04-18 18:04:30 +0200

    save error alert

--- src/constants/Errors.java
+++ src/constants/Errors.java
@@ -6,4 +6,5 @@ package constants;
 public class Errors {
     public static final String ERROR = "Error";
     public static final String CREATE_ERROR = "Failed to create a project";
+    public static final String SAVE_ERROR = "Failed to save the project";
 }

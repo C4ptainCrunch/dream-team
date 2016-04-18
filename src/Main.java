fe2ceb68db7968a59b9a6d62020de8d3ec7c9372
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-18 14:00:26 +0200

    Add a log formatter

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
 import gui.projectManagement.views.ProjectManagementView;
 import utils.Log;
 
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
 
 public class Main {
     private static final Logger logger = Log.getLogger(Main.class);
 
     public static void main(String... args) {
         Handler ch = new ConsoleHandler();
+        ch.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                Date date = new Date(logRecord.getMillis());
+                return String.format(
+                        "%s %s (%s:%s) : %s\n",
+                        new SimpleDateFormat("H:m:s").format(date),
+                        logRecord.getLevel(),
+                        logRecord.getLoggerName(),
+                        logRecord.getSourceMethodName(),
+                        logRecord.getMessage()
+                );
+            }
+        });
         ch.setLevel(Level.ALL);
+        for (Handler h: Logger.getLogger("").getHandlers()) {
+            Logger.getLogger("").removeHandler(h);
+        }
+
         Logger.getLogger("").addHandler(ch);
 
         logger.info("Starting project management view");

9ca41d6a29a9053fe51d13dcfb9d5d6f7c62f47b
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-19 13:42:43 +0200

    Move the logging setup to the Log util

--- src/utils/Log.java
+++ src/utils/Log.java
@@ -1,7 +1,8 @@
 package utils;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
 
 public class Log {
     public static Logger getLogger(Class c){
@@ -16,5 +17,29 @@ public class Log {
         Logger l = Logger.getLogger(name);
         l.setLevel(level);
         return l;
+    }
+
+    public static void init() {
+        Handler ch = new ConsoleHandler();
+        ch.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                Date date = new Date(logRecord.getMillis());
+                return String.format(
+                        "%s %s (%s:%s) : %s\n",
+                        new SimpleDateFormat("H:m:s").format(date),
+                        logRecord.getLevel(),
+                        logRecord.getLoggerName(),
+                        logRecord.getSourceMethodName(),
+                        logRecord.getMessage()
+                );
+            }
+        });
+        ch.setLevel(Level.ALL);
+        for (Handler h: Logger.getLogger("").getHandlers()) {
+            Logger.getLogger("").removeHandler(h);
+        }
+
+        Logger.getLogger("").addHandler(ch);
     }
 }

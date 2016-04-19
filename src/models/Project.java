5bcce6bd8bf3dbcaeb931169171dfd9fb31c18e9
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-19 12:57:15 +0200

    Add javadoc

--- src/models/Project.java
+++ src/models/Project.java
@@ -109,6 +109,14 @@ public class Project implements Comparable<Project>{
         return this.path.getFileName().toString();
     }
 
+    /**
+     * Saves the project to disk.
+     * It computes the diff between the actual version and the last one,
+     * writes the new history to disk and then writes the source to disk.
+     * It also adds the project to the recent projects list.
+     * @throws IOException when writing to the disk failed
+     * @throws ClassNotFoundException when the diff file is corrupted
+     */
     public void save()  throws IOException, ClassNotFoundException{
         List<Diff> diffs = null;
         try {
@@ -173,6 +181,12 @@ public class Project implements Comparable<Project>{
         RecentProjects.addProject(this);
     }
 
+    /**
+     * Gets the list of diffs from the diff file.
+     * @return A list of Diff objects
+     * @throws IOException : when reading the file failed
+     * @throws ClassNotFoundException : when the file is corrupted
+     */
     public List<Diff> getDiffs() throws IOException, ClassNotFoundException {
         FileInputStream fs = new FileInputStream(this.getDiffPath().toFile());
         ObjectInputStream os = new ObjectInputStream(fs);
@@ -183,6 +197,11 @@ public class Project implements Comparable<Project>{
         return diffs;
     }
 
+    /**
+     * Serializes the diffs to the diff file
+     * @param diffs
+     * @throws IOException when wrinting to the file failed
+     */
     public void writeDiffs(List<Diff> diffs) throws IOException {
         FileOutputStream fs = new FileOutputStream(this.getDiffPath().toFile());
         ObjectOutputStream os = new ObjectOutputStream(fs);
@@ -192,6 +211,11 @@ public class Project implements Comparable<Project>{
         fs.close();
     }
 
+    /**
+     * Writes the tikz source of the project to the save file
+     * @param  tikz : the tikz source
+     * @throws IOException when wrinting failed
+     */
     public void writeTikz(String tikz) throws IOException {
         PrintWriter sourceWriter = new PrintWriter(this.getTikzPath().toFile());
         sourceWriter.println(tikz);
@@ -207,7 +231,8 @@ public class Project implements Comparable<Project>{
     }
 
     /**
-     * Compares this Project with the given Project for order.
+     * Compares this Project with the given Project for order
+     * (uses the path for comparison).
      * @param other The project to be compared with
      * @return The order
      */
@@ -216,6 +241,13 @@ public class Project implements Comparable<Project>{
         return this.getPath().compareTo(other.getPath());
     }
 
+
+    /**
+     * Gets the date of the last change of the project
+     * @return Date the date
+     * @throws IOException when the diff file does not exist
+     * @throws ClassNotFoundException when the diff file is corrupted
+     */
     public Date getLastChange() throws IOException, ClassNotFoundException {
         List<Diff> diffs = this.getDiffs();
         if(diffs.size() == 0){

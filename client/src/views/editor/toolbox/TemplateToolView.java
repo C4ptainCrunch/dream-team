4bb983dbaea3a52448cdb70ca47ae52fa361d1a1
Thomas Herman <hermanthomas9@outlook.com>
2016-04-19 18:03:37 +0200

    1st version of Shapes/Templates tab (very ugly but functionnal, will probably be changed)

---
+++ client/src/views/editor/toolbox/TemplateToolView.java
@@ -0,0 +1,10 @@
+package views.editor.toolbox;
+
+import javax.swing.*;
+
+/**
+ * Created by mrmmtb on 19.04.16.
+ */
+public class TemplateToolView extends JPanel {
+
+}
--- client/src/views/editor/toolbox/ToolBoxView.java
+++ client/src/views/editor/toolbox/ToolBoxView.java
@@ -13,18 +13,33 @@ import models.tikz.TikzComponent;
 public class ToolBoxView extends JPanel {
 
     private static final int BOX_WIDTH = 200;
+    private static final String SHAPE_TAB = "<html>S<br>H<br>A<br>P<br>E<br>S</html>";
+    private static final String TEMPLATE_TAB = "<html>T<br>E<br>M<br>P<br>L<br>A<br>T<br>E<br>S</html>";
 
     private final ToolView tikzComponentCreator;
+    private final TemplateToolView tikzTemplateCreator;
+    private JTabbedPane tabbedPane;
 
     /**
      * Constructs a new view for the ToolBox
      */
     public ToolBoxView() {
         tikzComponentCreator = new ToolView();
+        tikzTemplateCreator = new TemplateToolView();
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setBorder(new BevelBorder(BevelBorder.RAISED));
-        this.add(tikzComponentCreator);
         this.setPreferredSize(new Dimension(BOX_WIDTH, this.getHeight()));
+        initTabs();
+    }
+
+    private void initTabs(){
+        this.tabbedPane = new JTabbedPane();
+        this.tabbedPane.setTabPlacement(JTabbedPane.LEFT);
+        this.tabbedPane.addTab(SHAPE_TAB, this.tikzComponentCreator);
+        this.tabbedPane.addTab(TEMPLATE_TAB, this.tikzTemplateCreator);
+
+        this.tabbedPane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
+        this.add(tabbedPane);
     }
 
     /**

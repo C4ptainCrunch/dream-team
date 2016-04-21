17c6f97648a97a538cd278f26afdbc0a05769074
Thomas Herman <hermanthomas9@outlook.com>
2016-04-21 15:32:22 +0200

    Updated User model with constructor

--- shared/src/models/users/User.java
+++ shared/src/models/users/User.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 @XmlRootElement
 public class User {
-    private String username;
     private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
     private List<String> documents;
+
 
     public User() {
         // JAX-RS needs a constructor without any parameters
@@ -17,6 +21,14 @@ public class User {
         this.id = id;
         this.username = username;
         this.documents = documents;
+    }
+
+    public User(int id, String username, String firstName, String lastName, String email) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     @XmlAttribute
@@ -29,6 +41,15 @@ public class User {
     public int getId() {
         return id;
     }
+
+    @XmlAttribute
+    public String getFirstName() { return firstName; }
+
+    @XmlAttribute
+    public String getLastName() { return lastName; }
+
+    @XmlAttribute
+    public String getEmail() { return email; }
 
     @XmlElementWrapper
     @XmlElement(name="document")

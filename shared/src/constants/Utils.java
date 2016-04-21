45c13f26d00c8deb3ef9ed9e8e99256a80c4a55e
André Madeira Cortes <amadeiracortes@gmail.com>
2016-04-21 12:19:43 +0200

    Adding more tests to Graph.toString() and to some utils (#34)

--- shared/src/constants/Utils.java
+++ shared/src/constants/Utils.java
@@ -7,7 +7,7 @@ public final class Utils {
     public static final String HISTORY_PATH = ".history";
     public static final String LINUX_PATH = "/.local/share/CreaTiKz";
     public static final String MAC_PATH = "/Library/Application Support/CreaTiKz";
-    public static final String WINDOWS_PATH_ONE = "C:\\Documents and Settings\\";
-    public static final String WINDOWS_PATH_TWO = "\\Application Data\\Local Settings\\GroupeUn\\CreaTiKz";
+    public static final String WINDOWS_PATH_ONE = "Documents and Settings/";
+    public static final String WINDOWS_PATH_TWO = "/Application Data/Local Settings/GroupeUn/CreaTiKz";
     public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
 }

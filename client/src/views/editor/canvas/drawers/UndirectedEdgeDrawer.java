43b8515311545911451560a2d695918dcfc637f4
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-10 10:26:23 +0200

    remove empty constructors

--- client/src/views/editor/canvas/drawers/UndirectedEdgeDrawer.java
+++ client/src/views/editor/canvas/drawers/UndirectedEdgeDrawer.java
@@ -1,7 +1,4 @@
 package views.editor.canvas.drawers;
 
 public class UndirectedEdgeDrawer extends EdgeDrawer {
-    public UndirectedEdgeDrawer() {
-        // this was left intentionally blank
-    }
 }

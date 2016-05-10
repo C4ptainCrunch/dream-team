97fb4e6ecf3a2cea80b8e4788ca495efc81e0198
acaccia <acaccia@ulb.ac.be>
2016-05-10 14:37:59 +0200

    missing javadoc: reloaded

--- shared/src/utils/DiffUtil.java
+++ shared/src/utils/DiffUtil.java
@@ -6,6 +6,13 @@ import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 
 public class DiffUtil {
 
+    /**
+     * create a diff between two string
+     * @param old previous string
+     * @param current current string
+     * @return a string representation of a diff
+     * @throws UnsupportedEncodingException
+     */
     public static String diff(String old, String current) throws UnsupportedEncodingException {
         DiffMatchPatch diff = new DiffMatchPatch();
         return diff.patchToText(diff.patchMake(current, old));

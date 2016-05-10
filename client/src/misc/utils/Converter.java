f5503610519ed4e38fbe06536bd21bb49dd7df8c
etnarek <etnarek@users.noreply.github.com>
2016-05-10 21:23:21 +0200

    Remove prints (#96)
    * Remove remaining debug prints in converter
    * use logger instead of System.(out|err).println in DAO

--- client/src/misc/utils/Converter.java
+++ client/src/misc/utils/Converter.java
@@ -8,8 +8,8 @@ import javax.swing.*;
 public class Converter {
     public static Point2D.Float tikz2swing(Point2D.Float tikzPosition, int panelWidth, int panelHeight) {
         int DPI = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
-        double positionX = (tikzPosition.getX() * 10 * DPI)/25.4;
-        double positionY = (tikzPosition.getY() * 10 * DPI)/25.4;
+        double positionX = (tikzPosition.getX() *  DPI)/2.54;
+        double positionY = (tikzPosition.getY() *  DPI)/2.54;
         int swingX = (int) (panelWidth / 2 + positionX);
         int swingY = (int) (panelHeight / 2 - positionY);
         return new Point2D.Float(swingX, swingY);
@@ -23,10 +23,8 @@ public class Converter {
         int DPI = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
         double tikzX = (swingPosition.getX() - panelWidth / 2);
         double tikzY = (panelHeight / 2 - swingPosition.getY());
-        tikzX = (tikzX * 25.4) / (DPI * 10);
-        tikzY = (tikzY * 25.4) / (DPI * 10);
-        System.out.println((float)tikzX);
-        System.out.println((float)tikzY);
+        tikzX = (tikzX * 2.54) / (DPI);
+        tikzY = (tikzY * 2.54) / (DPI);
         return new Point2D.Float((float)tikzX, (float)tikzY);
     }
 

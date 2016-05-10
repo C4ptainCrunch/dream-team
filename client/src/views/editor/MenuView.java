55d990fe447c33da9b54f1aad26dd26908bb511f
Aurelienvd <aurelienvandelft@hotmail.com>
2016-05-10 16:18:20 +0200

    save, undo and redo shortcuts (#89)

--- client/src/views/editor/MenuView.java
+++ client/src/views/editor/MenuView.java
@@ -6,6 +6,9 @@ import models.project.Project;
 import constants.GUI;
 import controllers.editor.MenuController;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 /**
  * Implementation of the View (from the MVC architectural pattern) for the Menu.
  * The Menu is the menu bar of the GUI.
@@ -30,6 +33,47 @@ public class MenuView extends JMenuBar {
         this.render();
     }
 
+    private void addShortcutItem(JMenu menu, Action action, String label, String key, int shortcut_key, int key_mask){
+        JMenuItem item = new JMenuItem(label);
+        item.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(shortcut_key, key_mask),label);
+        item.setAction(action);
+        item.getActionMap().put(key, action);
+        menu.add(item);
+    }
+
+    private void addUndoItem(JMenu menu){
+        String key = "Undo";
+        Action action = new AbstractAction(key) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                project.undo();
+            }
+        };
+        addShortcutItem(menu, action, GUI.MenuBar.UNDO, key, KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK);
+    }
+
+    private void addRedoItem(JMenu menu){
+        String key = "Redo";
+        Action action = new AbstractAction(key) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                project.redo();
+            }
+        };
+        addShortcutItem(menu, action, GUI.MenuBar.REDO, key, KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK);
+    }
+
+    private void addSaveItem(JMenu menu){
+        String key = "Save";
+        Action action = new AbstractAction(key) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                controller.save();
+            }
+        };
+        addShortcutItem(menu, action, GUI.MenuBar.SAVE, key, KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK);
+    }
+
     /**
      * Renders the view by setting the different buttons composing the menu
      */
@@ -37,9 +81,7 @@ public class MenuView extends JMenuBar {
         JMenu file_menu = new JMenu(GUI.MenuBar.FILE_MENU);
         this.add(file_menu);
 
-        JMenuItem save_item = new JMenuItem(GUI.MenuBar.SAVE);
-        save_item.addActionListener(e -> controller.save());
-        file_menu.add(save_item);
+        addSaveItem(file_menu);
 
         JMenuItem build_pdf = new JMenuItem(GUI.MenuBar.PDF);
         build_pdf.addActionListener(actionEvent -> controller.compileAndOpen());
@@ -58,13 +100,9 @@ public class MenuView extends JMenuBar {
         JMenu edit_menu = new JMenu(GUI.MenuBar.EDIT_MENU);
         this.add(edit_menu);
 
-        JMenuItem undo_item = new JMenuItem(GUI.MenuBar.UNDO);
-        undo_item.addActionListener(actionEvent -> project.undo());
-        edit_menu.add(undo_item);
+        addUndoItem(edit_menu);
 
-        JMenuItem redo_item = new JMenuItem(GUI.MenuBar.REDO);
-        redo_item.addActionListener(actionEvent -> project.redo());
-        edit_menu.add(redo_item);
+        addRedoItem(edit_menu);
 
         JMenu help_menu = new JMenu(GUI.MenuBar.HELP_MENU);
         this.add(help_menu);

3e24fc9e6909d4cbe11ead7efd358302c2db5c4a
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-05-10 17:55:46 +0200

    [add] gui full reader

--- client/src/constants/ClientPropertiesLoader.java
+++ client/src/constants/ClientPropertiesLoader.java
@@ -1,7 +1,10 @@
 package constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 import static constants.Errors.*;
+import static constants.GUI.*;
 
 /**
  * Created by jhellinckx on 10/05/16.
@@ -55,6 +58,76 @@ class GUIPropertiesReader implements PropertiesReader{
 
     @Override
     public void read(Properties properties) {
+        MenuBar.FILE_MENU = properties.getProperty("FILE_MENU");
+        MenuBar.SAVE = properties.getProperty("SAVE");
+        MenuBar.PDF = properties.getProperty("PDF");
+        MenuBar.EXIT = properties.getProperty("EXIT");
+        MenuBar.VIEW_MENU = properties.getProperty("VIEW_MENU");
+        MenuBar.GRID_VISIBILTY = properties.getProperty("GRID_VISIBILTY");
+        MenuBar.HELP_MENU = properties.getProperty("HELP_MENU");
+        MenuBar.HELP = properties.getProperty("HELP");
+        MenuBar.DIFF = properties.getProperty("DIFF");
+        MenuBar.APP_NAME = properties.getProperty("APP_NAME");
+        MenuBar.OPTIONS_MENU = properties.getProperty("OPTIONS_MENU");
+        MenuBar.COLOR_BLIND = properties.getProperty("COLOR_BLIND");
+        MenuBar.EDIT_MENU = properties.getProperty("EDIT_MENU");
+        MenuBar.UNDO = properties.getProperty("UNDO");
+        MenuBar.REDO = properties.getProperty("REDO");
+
+        ProjectManagement.CREATE_BUTTON = properties.getProperty("CREATE_BUTTON");
+        ProjectManagement.OPEN_BUTTON = properties.getProperty("OPEN_BUTTON");
+        ProjectManagement.RENAME_BUTTON = properties.getProperty("RENAME_BUTTON");
+        ProjectManagement.CREATE_PANEL = properties.getProperty("CREATE_PANEL");
+        ProjectManagement.IMPORT_PANEL = properties.getProperty("IMPORT_PANEL");
+        ProjectManagement.DROPDOWN_HEADER = properties.getProperty("DROPDOWN_HEADER");
+        ProjectManagement.BLANK_INFO_PANEL = properties.getProperty("BLANK_INFO_PANEL");
+        ProjectManagement.DIFF_TEXT = properties.getProperty("DIFF_TEXT");
+
+        LoginWindow.LOGIN_BUTTON = properties.getProperty("LOGIN_BUTTON");
+        LoginWindow.SIGNUP_BUTTON = properties.getProperty("SIGNUP_BUTTON");
+        LoginWindow.TOKEN_BUTTON = properties.getProperty("TOKEN_BUTTON");
+        LoginWindow.EDIT_BUTTON = properties.getProperty("EDIT_BUTTON");
+
+        TokenWindow.WIN_LABEL = properties.getProperty("WIN_LABEL");
+        TokenWindow.OK_BUTTON = properties.getProperty("OK_BUTTON");
+        TokenWindow.USER_LABEL = properties.getProperty("USER_LABEL");
+        TokenWindow.TOKEN_LABEL = properties.getProperty("TOKEN_LABEL");
+        TokenWindow.TOKEN_VALID = properties.getProperty("TOKEN_VALID");
+        TokenWindow.TOKEN_WRONG = properties.getProperty("TOKEN_WRONG");
+        
+        SignUp.FIELD_LABELS = new ArrayList<>(Arrays.asList(properties.getProperty("FIELD_LABELS").split("#")));
+
+        String[] sizesStrings = properties.getProperty("FIELD_SIZES").split("#");
+        SignUp.FIELD_SIZES = new ArrayList<>();
+        for(int i = 0; i < sizesStrings.length; ++i){ SignUp.FIELD_SIZES.add(Integer.valueOf(sizesStrings[i])) }
+
+        SignUp.PASSWORD_LABEL = properties.getProperty("PASSWORD_LABEL");
+        SignUp.OK_BUTTON = properties.getProperty("OK_BUTTON");
+        SignUp.CANCEL_BUTTON = properties.getProperty("CANCEL_BUTTON");
+        SignUp.NAMES_REGEX = properties.getProperty("NAMES_REGEX");
+        SignUp.USERNAME_REGEX = properties.getProperty("USERNAME_REGEX");
+
+        Drawing.ARROW_LENGTH = Double.valueOf(properties.getProperty("ARROW_LENGTH"));
+        Drawing.ARROW_ANGLE = Double.valueOf(properties.getProperty("ARROW_ANGLE"));
+
+        Tabs.HAPE_TAB = properties.getProperty("HAPE_TAB");
+        Tabs.EMPLATE_TAB = properties.getProperty("EMPLATE_TAB");
+
+        String[] rgbStrings = properties.getProperty("BKG_COLOR").split("#");
+        int[] rgb = new int[4];
+        for(int i = 0; i < rgbStrings.length; ++i){ rgb[i] = Integer.parseInt(rgbStrings[i]); }
+        Selection.BKG_COLOR = new Color(rgb[0], rgb[1], rgb[2], rgb[3]);
+
+        Template.DIR = Dirs.getDataDir().resolve(properties.getProperty("TEMPLATE_DIR")).toString();
+
+        TextArea.DEFAULT_THEME = properties.getProperty("DEFAULT_THEME");
+        TextArea.DEFAULT_COLOR_BLINDNESS_THEME = properties.getProperty("DEFAULT_COLOR_BLINDNESS_THEME");
+    
+
+    public static final class TextArea {
+        public static String DEFAULT_THEME = "syntax_themes/default.xml";
+        public static String DEFAULT_COLOR_BLINDNESS_THEME = "syntax_themes/default_color_blindness.xml";
+    }
 
     }
 }

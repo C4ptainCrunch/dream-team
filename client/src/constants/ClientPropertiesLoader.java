d379bb2dce5166fe054ec29a49f7698e2ceadfff
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-05-10 17:02:44 +0200

    [add] errors client reader

--- client/src/constants/ClientPropertiesLoader.java
+++ client/src/constants/ClientPropertiesLoader.java
@@ -1,11 +1,16 @@
 package constants;
 
 import java.util.Properties;
+import static constants.Errors.*;
 
 /**
  * Created by jhellinckx on 10/05/16.
  */
 public class ClientPropertiesLoader extends PropertiesLoader {
+    private static final String ERRORS_PROPERTIES_FILENAME = "errors.properties";
+    private static final String GUI_PROPERTIES_FILENAME = "gui.properties";
+    private static final String WARNING_PROPERTIES_FILENAME = "warning.properties";
+
     public static void loadAll(){
         PropertiesLoader.loadAll();
         loadErrorsProperties();
@@ -14,22 +19,35 @@ public class ClientPropertiesLoader extends PropertiesLoader {
     }
 
     public static void loadErrorsProperties(){
-
+        PropertiesLoader.load(ERRORS_PROPERTIES_FILENAME, new ErrorsPropertiesReader());
     }
 
     public static void loadGUIProperties(){
-
+        PropertiesLoader.load(GUI_PROPERTIES_FILENAME, new GUIPropertiesReader());
     }
 
     public static void loadWarningProperties(){
-
+        PropertiesLoader.load(WARNING_PROPERTIES_FILENAME, new WarningPropertiesReader());
     }
 }
 
 class ErrorsPropertiesReader implements PropertiesReader{
+    
     @Override
     public void read(Properties properties){
-
+        ERROR = properties.getProperty("ERROR");
+        CREATE_ERROR = properties.getProperty("CREATE_ERROR");
+        SAVE_ERROR = properties.getProperty("SAVE_ERROR");
+        FILL_VIEW_ERROR = properties.getProperty("FILL_VIEW_ERROR");
+        OPEN_ERROR = properties.getProperty("OPEN_ERROR");
+        RENAME_ERROR = properties.getProperty("RENAME_ERROR");
+        CREATE_TEMPLATE_DIR_ERROR = properties.getProperty("CREATE_TEMPLATE_DIR_ERROR");
+        SAVE_TEMPLATE_ERROR = properties.getProperty("SAVE_TEMPLATE_ERROR");
+        LOAD_TEMPLATES_ERROR = properties.getProperty("LOAD_TEMPLATES_ERROR");
+        LOAD_TEXT_AREA_ERROR = properties.getProperty("LOAD_TEXT_AREA_ERROR");
+        ACTIVE_ACCOUNT_FIRST = properties.getProperty("ACTIVE_ACCOUNT_FIRST");
+        LOGIN_FAILED = properties.getProperty("LOGIN_FAILED");
+        SIGNUP_FAILED = properties.getProperty("SIGNUP_FAILED");
     }
 }
 
--- client/src/constants/Errors.java
+++ client/src/constants/Errors.java
@@ -4,17 +4,17 @@ package constants;
  * Created by mrmmtb on 18.04.16.
  */
 public class Errors {
-    public static final String ERROR = "Error";
-    public static final String CREATE_ERROR = "Failed to create a project";
-    public static final String SAVE_ERROR = "Failed to save the project";
-    public static final String FILL_VIEW_ERROR = "Failed to read the diff file";
-    public static final String OPEN_ERROR = "Failed to open project";
-    public static final String RENAME_ERROR = "Failed to rename project";
-    public static final String CREATE_TEMPLATE_DIR_ERROR = "Failed to create the template directory";
-    public static final String SAVE_TEMPLATE_ERROR = "Failed to save the template";
-    public static final String LOAD_TEMPLATES_ERROR = "Failed to load templates";
-    public static final String LOAD_TEXT_AREA_ERROR = "Failed to load text theme";
-    public static final String ACTIVE_ACCOUNT_FIRST = "Activate your account first to be able to log in";
-    public static final String LOGIN_FAILED = "Username-password combination did not match";
-    public static final String SIGNUP_FAILED = "Failed to create a new account";
+    public static String ERROR = "Error";
+    public static String CREATE_ERROR = "Failed to create a project";
+    public static String SAVE_ERROR = "Failed to save the project";
+    public static String FILL_VIEW_ERROR = "Failed to read the diff file";
+    public static String OPEN_ERROR = "Failed to open project";
+    public static String RENAME_ERROR = "Failed to rename project";
+    public static String CREATE_TEMPLATE_DIR_ERROR = "Failed to create the template directory";
+    public static String SAVE_TEMPLATE_ERROR = "Failed to save the template";
+    public static String LOAD_TEMPLATES_ERROR = "Failed to load templates";
+    public static String LOAD_TEXT_AREA_ERROR = "Failed to load text theme";
+    public static String ACTIVE_ACCOUNT_FIRST = "Activate your account first to be able to log in";
+    public static String LOGIN_FAILED = "Username-password combination did not match";
+    public static String SIGNUP_FAILED = "Failed to create a new account";
 }

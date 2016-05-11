5885db016ab8f81d697fc4acf2a8ba1e3fe24f5a
Aurelien van Delft <avdelft@ulb.ac.be>
2016-05-11 20:01:50 +0200

    (wip) Project test

--- shared/tests/models/project/ProjectTest.java
+++ shared/tests/models/project/ProjectTest.java
@@ -1,10 +1,30 @@
 package models.project;
 
+import static org.junit.Assert.assertEquals;
+
 import utils.SharedTest;
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 
 public class ProjectTest extends SharedTest{
+
+    private Project project;
+    private Diagram diagram;
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
     @Test
     public void getDiagramSource() throws Exception {
 

3bc0a4997780a6892f6e85cbd7742dd880c3a1a8
Aurelien van Delft <avdelft@ulb.ac.be>
2016-05-11 22:32:25 +0200

    test project last change

--- shared/tests/models/project/ProjectTest.java
+++ shared/tests/models/project/ProjectTest.java
@@ -9,25 +9,43 @@ import utils.SharedTest;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.*;
 
 
 public class ProjectTest extends SharedTest {
 
-    private final String diagram_name = "test_diagram";
+    private final long time_diff = 60000L;
+    private final String first_diagram_name = "test_diagram";
+    private final String second_diagram_name = "second_diagram";
 
     private Project project;
     private Diagram diagram;
     private TikzGraph graph;
 
+    private Date getLaterTime(){
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+        date.setTime(date.getTime()+time_diff);
+       return date;
+    }
+
+    private void writeDiff(List<Diff> diff) throws Exception{
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bs);
+        os.writeObject(diff);
+
+        project.writeDiff(second_diagram_name, bs.toByteArray());
+        os.close();
+        bs.close();
+    }
+
     @Before
     public void setUp() throws Exception {
         project = new Project();
-        diagram = new Diagram(diagram_name, project);
+        diagram = new Diagram(first_diagram_name, project);
         graph = new TikzGraph();
         TikzCircle circle1 = new TikzCircle();
         TikzCircle circle2 = new TikzCircle();
@@ -45,12 +63,20 @@ public class ProjectTest extends SharedTest {
     @Test
     public void testWriteSource() throws Exception {
         String base_source = graph.toString();
-        project.writeSource(diagram_name, base_source);
-        assertEquals(base_source, project.getDiagramSource(diagram_name));
+        project.writeSource(first_diagram_name, base_source);
+        assertEquals(base_source, project.getDiagramSource(first_diagram_name));
     }
 
     @Test
     public void testLastChange() throws Exception {
-        
+        project.writeSource(first_diagram_name, graph.toString());
+        diagram.save();
+        Diagram second_diagram = new Diagram(second_diagram_name, project);
+        second_diagram.save();
+        Date date = getLaterTime();
+        Diff new_diff = new Diff(date, "");
+        List<Diff> diffs = new ArrayList<>(Arrays.asList(new_diff));
+        writeDiff(diffs);
+        assertEquals(date, project.getLastChange());
     }
 }

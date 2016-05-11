a0d378cdba948abbc32658f855d9149f7e85c3d6
rvdmarck <raphvdm@gmail.com>
2016-05-11 15:41:48 +0200

    remove default value properties shared

--- shared/src/constants/Models.java
+++ shared/src/constants/Models.java
@@ -5,25 +5,25 @@ import java.util.ResourceBundle;
 
 public final class Models {
     public static final class DEFAULT {
-        public static Color COLOR = Color.black;
-        public static int STROKE = 1;
-        public static int X = 0;
-        public static int Y = 0;
-        public static String LABEL = "";
-        public static int LENGTH = 100;
-        public static int SIDES = 3;
-        public static Color BACKGROUND_COLOR = Color.white;
-        public static int EDGE_X_LENGTH = 100;
+        public static Color COLOR;
+        public static int STROKE;
+        public static int X;
+        public static int Y;
+        public static String LABEL;
+        public static int LENGTH;
+        public static int SIDES;
+        public static Color BACKGROUND_COLOR;
+        public static int EDGE_X_LENGTH;
     }
 
     public static class Project {
-        public static String SAVE_FILE = "save.tikz";
-        public static String DIFF_FILE = "diff";
+        public static String SAVE_FILE;
+        public static String DIFF_FILE;
     }
 
     public static class Graph {
-        public static String LATEX_PRELUDE = "\\documentclass{article}\n\\usepackage{tikz}\n\\usetikzlibrary{shapes.geometric}\n\\begin{document}\n\\begin{tikzpicture}\n";
-        public static String LATEX_POSTLUDE = "\\end{tikzpicture}\n\\end{document}\n";
+        public static String LATEX_PRELUDE;
+        public static String LATEX_POSTLUDE;
     }
 
 }

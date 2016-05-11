49264948b3e48bfc7fe51e34a7bcb1549109788d
Thomas Herman <hermanthomas9@outlook.com>
2016-05-11 15:45:44 +0200

    removed default value for server constants

--- server/src/constants/Database.java
+++ server/src/constants/Database.java
@@ -1,9 +1,9 @@
 package constants;
 
 public final class Database {
-    public static String DB_HOME_TEST_DIR = "server";
-    public static String DB_DIR = "server/database";
-    public static String DB_FILE = DB_DIR + "/CreaTikZ.db";
-    public static String SQLITE_JDBC = "org.sqlite.JDBC";
-    public static String SQLITE_DB_CONNECTION = "jdbc:sqlite:"+ DB_FILE;
+    public static String DB_HOME_TEST_DIR;
+    public static String DB_DIR;
+    public static String DB_FILE;
+    public static String SQLITE_JDBC;
+    public static String SQLITE_DB_CONNECTION;
 }

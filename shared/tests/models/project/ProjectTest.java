be67602cec9f9d0bd3e03eeaa517eacba2fff9cc
Aurelien van Delft <avdelft@ulb.ac.be>
2016-05-11 22:45:03 +0200

    add move test

--- shared/tests/models/project/ProjectTest.java
+++ shared/tests/models/project/ProjectTest.java
@@ -5,13 +5,19 @@ import static org.junit.Assert.assertEquals;
 import models.tikz.TikzCircle;
 import models.tikz.TikzGraph;
 import models.tikz.TikzUndirectedEdge;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 import utils.SharedTest;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 
@@ -20,6 +26,7 @@ public class ProjectTest extends SharedTest {
     private final long time_diff = 60000L;
     private final String first_diagram_name = "test_diagram";
     private final String second_diagram_name = "second_diagram";
+    private final String new_project_path = "second_name";
 
     private Project project;
     private Diagram diagram;
@@ -78,5 +85,13 @@ public class ProjectTest extends SharedTest {
         List<Diff> diffs = new ArrayList<>(Arrays.asList(new_diff));
         writeDiff(diffs);
         assertEquals(date, project.getLastChange());
+    }
+
+    @Test
+    public void testMoveProject() throws Exception {
+        Path dir = Files.createTempDirectory("test");
+        Path p = Paths.get(dir.toString(), new_project_path);
+        project.move(p.toFile());
+        assertEquals(p, project.getPath());
     }
 }

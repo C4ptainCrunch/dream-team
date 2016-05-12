e7f0f4f1c806ccceb1537c805bd3b6af327f49e5
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-12 13:07:42 +0200

    Generic entity list instead of list

--- server/src/endpoints/ProjectEndpoint.java
+++ server/src/endpoints/ProjectEndpoint.java
@@ -13,10 +13,7 @@ import utils.Log;
 
 import javax.ws.rs.*;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,10 +33,10 @@ public class ProjectEndpoint {
     @Secured
     @Path("/list")
     @Produces("application/xml")
-    public List<Project> getProjectList(@Context SecurityContext securityContext){
+    public GenericEntity<List<Project>> getProjectList(@Context SecurityContext securityContext){
         String username = securityContext.getUserPrincipal().getName();
         User user = usersDAO.findByUsername(username);
-        return projectsDAO.getAllReadableProject(user.getId());
+        return new GenericEntity<List<Project>>(projectsDAO.getAllReadableProject(user.getId())) {};
     }
 
     @PUT

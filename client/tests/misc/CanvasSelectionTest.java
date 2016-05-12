1aaf89bc67a2feae2f45834942541e9da053bae0
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-05-12 12:05:32 +0200

    [add] skeleton for canvasselecitontest

---
+++ client/tests/misc/CanvasSelectionTest.java
@@ -0,0 +1,38 @@
+package misc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhellinckx on 12/05/16.
+ */
+public class CanvasSelectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testResize() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSelectionRectangle() throws Exception {
+
+    }
+}

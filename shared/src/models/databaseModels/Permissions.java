d91e52eacf80a632774ecfc624e510b6702d3993
etnarek <etnarek@users.noreply.github.com>
2016-05-12 19:32:23 +0200

    Add username and DAOFactorySingeton (#117)
    * change remaining projectId to projectUid in permissions
    
    * DAOFactory is now a singleton and Project+Permissions have username

--- shared/src/models/databaseModels/Permissions.java
+++ shared/src/models/databaseModels/Permissions.java
@@ -1,26 +1,31 @@
 package models.databaseModels;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Created by acaccia on 11/05/16.
  */
+@XmlRootElement
 public class Permissions {
-    private int projectID, userID;
+    private String username;
+    private String projectUID;
+    private int userID;
     private boolean writable, readable;
 
     public Permissions() {}
 
-    public Permissions(int projectID, int userID, boolean write, boolean read) {
-        this.projectID = projectID;
+    public Permissions(String projectUID, int userID, boolean write, boolean read, String username) {
+        this.projectUID = projectUID;
         this.userID = userID;
         this.writable = write;
         this.readable = read;
+        this.username = username;
     }
 
     @XmlAttribute
-    public int getProjectID() {
-        return projectID;
+    public String getProjectUID() {
+        return projectUID;
     }
 
     @XmlAttribute
@@ -36,5 +41,30 @@ public class Permissions {
     @XmlAttribute
     public boolean isReadable() {
         return readable;
+    }
+
+    public void setProjectUID(String projectUID) {
+        this.projectUID = projectUID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setWritable(boolean writable) {
+        this.writable = writable;
+    }
+
+    public void setReadable(boolean readable) {
+        this.readable = readable;
+    }
+
+    @XmlAttribute
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }

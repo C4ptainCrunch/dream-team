c3d6a8ff347901227e6b1895d0c623933e1208f2
jhellinckx <jerhelli@ulb.ac.be>
2016-05-12 17:36:29 +0200

    Client tests (#113)
    * ToolModel tests
    
    * [add] skeleton for canvasselecitontest
    
    * Canvas Selection test
    
    * [add] Converter tests
    
    * SourceController test
    
    * [add] AttributeChooserTest
    
    * [add] server dependency for client  test
    
    * [add] PreviewControllerTest
    
    * [add] SelectorControllerTest
    
    * [add] CanvasControllerTest base
    
    * [add] contains/isempty methods in graph
    
    * [add] tests for new graph methods
    
    * [add] other canvascontroller tests
    
    * [add] final canvascontroller test
    
    * [add] windows dependency in render
    
    * [add] windows dependency in render v2
    
    * [wip] trying to fix travis
    
    * [wip] TRAVIS PLS
    
    * [PLS] PLS

---
+++ client/tests/controllers/editor/toolbox/SelectorControllerTest.java
@@ -0,0 +1,48 @@
+package controllers.editor.toolbox;
+
+import misc.utils.ClientTest;
+import models.ToolModel;
+import models.tikz.TikzCircle;
+import models.tikz.TikzComponent;
+import models.tikz.TikzNode;
+import models.tikz.TikzRectangle;
+import org.junit.Before;
+import org.junit.Test;
+import views.editor.toolbox.SelectorView;
+import views.editor.toolbox.ToolView;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhellinckx on 12/05/16.
+ */
+public class SelectorControllerTest extends ClientTest {
+    private SelectorController controller;
+    private TikzNode node;
+    private ToolModel model;
+    private SelectorView view;
+    private ToolView parentView;
+
+    @Before
+    public void setUp(){
+        node = new TikzCircle();
+        node.setStrokeColor(Color.blue);
+        node.setLabel("its a prank");
+        model = new ToolModel(node);
+        parentView = new ToolView();
+        view = new SelectorView(parentView, model);
+        controller = new SelectorController(view, model);
+    }
+
+
+    @Test
+    public void testItemSelected() throws Exception {
+        TikzCircle selectedNode = new TikzCircle();
+        controller.itemSelected(selectedNode);
+        TikzComponent actualComp = model.getComponentClone();
+        actualComp.setReference(selectedNode.getReference());
+        assertEquals(actualComp.toString(), selectedNode.toString());
+    }
+}
